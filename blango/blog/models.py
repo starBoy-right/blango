c435ce4fc7155bcc5a76651ea7479223c5676bdb
starBoy-right <romiwahyudihasibuan@gmail.com>
2026-09-05 07:53:26 +0000

    finish generic relationships

--- blango/blog/models.py
+++ blango/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
 
 
@@ -10,6 +10,15 @@ class Tag(models.Model):
   def __str__(self):
     return self.value
 
+
+class Comment(models.Model):
+  creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  content = models.TextField()
+  content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+  object_id = models.PositiveIntegerField()
+  content_object = GenericForeignKey("content_type", "object_id")
+  created_at = models.DateTimeField(auto_now_add=True)
+  modified_at = models.DateTimeField(auto_now=True)
 
 class Post(models.Model):
   author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
@@ -21,15 +30,8 @@ class Post(models.Model):
   summary = models.TextField(max_length=500)
   content = models.TextField()
   tags = models.ManyToManyField(Tag, related_name="posts")
+  comment = GenericRelation(Comment)
 
   def __str__(self):
     return self.title
   
-
-class Comment(models.Model):
-  creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-  content = models.TextField()
-  content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-  object_id = models.PositiveIntegerField()
-  content_object = GenericForeignKey("content_type", "object_id")
-
